eb36bff34320c4096d378a2ff9b4a07383fc2719
Kaan Erdem <ke2365@columbia.edu>
2024-04-02 21:25:39 -0400

    Updating Layout
    Updated board & restart button layout. Centered all figures on the page.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import Board from './functions.js';
 
 function App() {
   
-  function restart(){
-    window.location.reload();
-  }
-
   return (
-    <>
-      <h1>Hello World!</h1>
-      <Board/>
-      <button type="button" onClick={ restart }> <span>Restart Game</span> </button> 
-    </>
+    <div className="board-container">
+      <div>
+        <h1>Tic-Tac-Toe Game</h1>
+        <Board/>
+      </div>
+    </div>
   );
   
 }
--- src/functions.js
+++ src/functions.js
@@ -37,22 +37,25 @@ export default function Board(){
 
     return(
         <>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>   
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>   
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>        
+            <div className='board'>
+                <div className="board-row">
+                    <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>   
+                    <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>   
+                    <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>        
+                </div>
+                <div className="board-row">
+                    <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>   
+                    <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>   
+                    <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>        
+                </div>
+                <div className="board-row">
+                    <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>   
+                    <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>   
+                    <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>        
+                </div>
+                <div className='result'> {result} </div>
+                <button type="button" onClick={ restart }> <span>Restart Game</span> </button>
             </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>   
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>   
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>        
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>   
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>   
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>        
-            </div>
-            <div className='result'> {result} </div>
         </>
     );
 }
@@ -76,3 +79,6 @@ function calculateWinner(squares){
       }
     return null;
 }
+function restart(){
+    window.location.reload();
+  }
